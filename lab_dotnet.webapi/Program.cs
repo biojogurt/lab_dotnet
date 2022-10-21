0262b8f658b91b716d8d8f47ff9d08b607ef9f9f
biojogurt <anathebeequeen@gmail.com>
2022-10-21 12:29:29 +0300

    create webapi

---
+++ lab_dotnet.webapi/Program.cs
@@ -0,0 +1,41 @@
+using lab_dotnet.webapi.AppConfigure.ApplicationExtensions;
+using lab_dotnet.webapi.AppConfigure.ServicesExtensions;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.AddSerilogConfiguration();
+builder.Services.AddVersioningConfiguration();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerConfiguration();
+
+var app = builder.Build();
+
+app.UseSerilogConfiguration();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerConfiguration();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+try
+{
+    Log.Information("Application starting...");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Error("Application finished with error {error}", ex);
+}
+finally
+{
+    Log.Information("Application stopped");
+    Log.CloseAndFlush();
+}
